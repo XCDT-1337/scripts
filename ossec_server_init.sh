44d702aca65efbb25c29ae4f6e2afa9ad4f05e9e
Peter Krasinski <pak2014@gmail.com>
2017-03-07 15:39:11 -0500

    added some bash scripts for ossec and system spin up
    some scripts I made long ago

---
+++ ossec_server_init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#assumes a root session
+cd /root/
+echo "installing required packages"
+apt-get install -y build-essential make libssl-dev git
+apt-get install -y mysql-server libmysqlclient-dev mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+echo "accept the options"
+mysql_secure_installation
+/etc/init.d/apache2 restart
+/etc/init.d/mysql restart
+echo "installing ossec-hids"
+wget https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
+tar -xf ossec-hids-2.8.3.tar.gz
+cd ossec-hids-2.8.3
+cd src
+make setdb
+cd ../
+echo "configure and copy info"
+./install.sh
+/var/ossec/bin/ossec-control restart
+echo "setup mysql DB 1. > create database ossec; 2. grant INSERT,SELECT,UPDATE,CREATE,DELETE,EXECUTE on ossec.* to ossec_u; \n3. set password for ossec_u = PASSWORD('Passw0rd'); 4. flush privileges; 5;. quit;"
+mysql -u root -p
+echo "input mysql password"
+read -rsp mysqlpass
+mysql -u root -p $mysqlpass < src/os_dbd/mysql.schema
+sed -e "\$a\n<ossec_config>\n\t<database_output>\n\t\t<hostname>127.0.0.1</hostname>\n\t\t<username>ossec_u</username>\n\t\t<password>Passw0rd</password>\n\t\t<database>ossec</database>\n\t\t<type>mysql</type>\n\t</database_output>/n</ossec_config>"
+/var/ossec/bin/ossec-control enable database
+/var/ossec/bin/ossec-control restart
+echo "installing web UI"
+wget https://github.com/ossec/ossec-wui/archive/0.9.tar.gz
+tar -xf ossec-wui-0.9.tar.gz
+mkdir -p /var/www/html/ossec/tmp/
+mv ossec-wui-0.9/* /var/www/html/ossec/
+chown www-data:www-data /var/www/html/ossec/tmp/
+chmod 666 /var/www/html/ossec/tmp
+usermod -a -G ossec www-data
---
+++ sys_init.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#Step I
+#assumes you have a session as root
+set -x
+trap "echo hit return;read x" DEBUG
+function randpw {
+	tr -dc 'A-Za-z0-9-_!@#$%^&*()_+{}|:<>=?' < /dev/urandom | head -c 32
+}
+
+grep wikus /etc/shadow #checks for C@C supervisory user wikus
+if [ $? -eq 0 ]
+	then
+    	deluser wikus
+		delgroup wikus
+		rm -rf /home/wikus
+fi
+
+apt-get update && apt-get -y upgrade #update/upgrade the system
+apt-get install sudo rsync
+
+ROOT_PASS=$(randpw)
+echo "New root password: $ROOT_PASS" #set root pass
+printf 'root: %s\n' "$ROOT_PASS" >> /root/script_info.txt
+echo "root:$ROOT_PASS" | chpasswd
+
+echo "Creating sudo-user eyes"
+EYES_PASS=$(randpw)
+printf 'eyes: %s\n' "$EYES_PASS" >> /root/script_info.txt
+adduser --disable-password --gecos eyes && echo "eyes:$EYES_PASS" | chpasswd
+adduser eyes sudo
+#login as sudo-user
+echo "Switching user"
+echo $EYES_PASS | su eyes
+
+echo "Performing ssh hardening"
+cd /etc/ssh/
+sudo mkdir default_deb_keys
+mv ssh_host_* default_deb_keys/ #backup default ssh keys
+dpkg-reconfigure openssh-server #chage keys
+cd
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup #backup sshd_config
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+sudo sed -i 's/Port 22/Port 7707/g' /etc/ssh/sshd_config #change ssh port to 7707 and disable sshroot
+sudo systemctl restart sshd
+
+echo "Setting up 2-Factor Authentication"
+sudo apt-get install -y libpam0g-dev make gcc
+sudo wget http://google-authenticator.googlecode.com/files/libpam-google-authenticator-1.0-source.tar.bz2
+sudo tar -xvf libpam-google-authenticator-1.0-source.tar.bz2
+cd libpam-google-authenticator-1.0/
+sudo make && make install
+echo "you take over and write down the codes, ain't nobody got time for that"
+google-authenticator
+echo "get those codes"
+read -rsp $'Press any key to continue...\n' -n1 keys
+sudo sed 's/# \/etc\/security\/pam_env.conf./\nauth       required     pam_google_authenticator.so' /etc/pam.d/sshd
+sudo sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+IP=$(netstat -ant | grep ESTABLISHED | cut -d 1)
+echo "input your local user \n"
+read -rsp lusr
+echo "\nis this your ip: $IP ? y/n"
+read -rsp ans
+if [ $ans == "y" ] then;
+	echo "sending script_info.txt to your ~\n"
+else if [ $ans == "n" ] then;
+	echo "input your ip address\n"
+	read -rsp ip
+	IP=$ip
+	fi
+fi
+echo $ROOT_PASS | sudo -i
+rsync -rv /root/script_info.txt $lusr@$IP:/~
