4897a13153e167b8a1265280988ef877e8be73f2
Drew Malzahn <drew.malzahn@gmail.com>
2018-03-15 23:49:10 -0400

    Moved docs to other firewall

--- app/ossec/ossec_server_init.sh
+++ app/ossec/ossec_server_init.sh
@@ -2,7 +2,7 @@
 #assumes a root session
 cd /root/
 echo "installing required packages"
-apt-get install -y build-essential make libssl-dev git
+apt-get install -y build-essential make libssl-dev git strings
 apt-get install -y mysql-server libmysqlclient-dev mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 echo "accept the options"
 mysql_secure_installation
